c01dde294fea04bbba96989d4cc8b3f40b677af1
IT <IT@DESKTOP-C7JRU9B>
2025-12-12 11:09:09 +0330

    Add project files.

---
+++ 00.FrameWork/Framework.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Domain.Entities
+{
+    public abstract class BaseEntity<TId> where TId : IEquatable<TId>
+    {
+        public TId Id { get; protected set; }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as BaseEntity<TId>;
+
+            if (ReferenceEquals(other, null))
+                return false;
+
+            if (ReferenceEquals(this, other))
+                return true;
+
+            if (GetType() != other.GetType())
+                return false;
+
+            if (Id == null || other.Id == null)
+                return false;
+
+            return Id.Equals(other.Id);
+        }
+    }
+}
