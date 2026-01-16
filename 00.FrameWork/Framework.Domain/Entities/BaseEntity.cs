1baf5b9372c3e72375bfb7bd3d2c26d18ea579e6
ali mohamadi <ali.mohamadi.1988@gmail.com>
2026-01-16 21:43:07 +0330

    add base entity

--- 00.FrameWork/Framework.Domain/Entities/BaseEntity.cs
+++ 00.FrameWork/Framework.Domain/Entities/BaseEntity.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Framework.Domain.ValueObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,27 +7,57 @@ using System.Threading.Tasks;
 
 namespace Framework.Domain.Entities
 {
-    public abstract class BaseEntity<TId> where TId : IEquatable<TId>
+    public abstract class Entity<TId> : IAuditableEntity
+              where TId : struct,
+              IComparable,
+              IComparable<TId>,
+              IConvertible,
+              IEquatable<TId>,
+              IFormattable
     {
+        /// <summary>
+        /// شناسه عددی Entityها
+        /// صرفا برای ذخیره در دیتابیس و سادگی کار مورد استفاده قرار بگیرید.
+        /// </summary>
         public TId Id { get; protected set; }
 
-        public override bool Equals(object obj)
+        /// <summary>
+        /// شناسه Entity
+        /// شناسه اصلی Entity که در همه جا باید مورد استفاده قرار گیرد BusinessId است.
+        /// تمامی ارتباطات به کمک این شناسه باید برقرار شود.
+        /// </summary>
+        public BusinessId BusinessId { get; protected set; } = BusinessId.FromGuid(Guid.NewGuid());
+
+        /// <summary>
+        /// سازنده پیش‌فرض به صورت Protected تعریف شده است.
+        /// با توجه به اینکه این نیاز است هنگام ساخت خواص اساسی Entity ایجاد شود، هیچ شی بدون پر کردن این خواص نباید ایجاد شود.
+        /// بار جلو گیری از این مورد برای همه Entityها باید سازنده‌هایی تعریف شود که مقدار ورودی دارند.
+        /// برای اینکه بتوان از همین Entityها برای فرایند ذخیره سازی و بازیابی از دیتابیس به کمک ORMها استفاده کرد، ضروری است که سازنده پیش‌فرض با سطح دسترسی بالا مثل Protected یا Private ایجاد شود.
+        /// </summary>
+        protected Entity() { }
+
+
+        #region Equality Check
+        public bool Equals(Entity<TId>? other) => this == other;
+        public override bool Equals(object? obj) =>
+             obj is Entity<TId> otherObject && Id.Equals(otherObject.Id);
+
+        public override int GetHashCode() => Id.GetHashCode();
+        public static bool operator ==(Entity<TId> left, Entity<TId> right)
         {
-            var other = obj as BaseEntity<TId>;
-
-            if (ReferenceEquals(other, null))
-                return false;
-
-            if (ReferenceEquals(this, other))
+            if (left is null && right is null)
                 return true;
 
-            if (GetType() != other.GetType())
+            if (left is null || right is null)
                 return false;
 
-            if (Id == null || other.Id == null)
-                return false;
-
-            return Id.Equals(other.Id);
+            return left.Equals(right);
         }
+
+        public static bool operator !=(Entity<TId> left, Entity<TId> right)
+            => !(right == left);
+
+        #endregion
     }
+
 }
