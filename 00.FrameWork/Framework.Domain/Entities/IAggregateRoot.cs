673dec5fcf575e00eb517992798dcd0e7dec52ff
ali mohamadi <ali.mohamadi.1988@gmail.com>
2026-01-16 21:57:34 +0330

    add aggregateroot

---
+++ 00.FrameWork/Framework.Domain/Entities/IAggregateRoot.cs
@@ -0,0 +1,14 @@
+﻿using Framework.Domain.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Domain.Entities;
+
+public interface IAggregateRoot
+{
+    void ClearEvents();
+    IEnumerable<IDomainEvent> GetEvents();
+}
